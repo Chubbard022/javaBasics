ca152ca13dbf35dc8ce233716181718153a38803
Chubbard022 <chubbard022@gmail.com>
2019-05-24 09:43:56 -0700

    hello world displaying within java

---
+++ introduction.java
@@ -0,0 +1,8 @@
+import java.io.Console;
+
+public class introduction{
+    public static void main(String[] args){
+        Console console = System.console();
+        console.printf("hell, my name is Curtis ");
+    }
+}
