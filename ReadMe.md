b61d89b2081809a6d87e562ef7db3674d30a2fec
Chubbard022 <chubbard022@gmail.com>
2019-05-24 16:32:55 -0700

    using multiple strings in java

--- ReadMe.md
+++ ReadMe.md
@@ -3,3 +3,7 @@ To compile code --> javac [filename]
 Data types within Java --> string,
 
 format specifier- %s
+
+print a line of code --> console.printf
+
+input from user --> console.readLine
--- introduction.java
+++ introduction.java
@@ -4,7 +4,8 @@ public class introduction{
     public static void main(String[] args){
         Console console = System.console();
 
-        String firstName = "Curtis";
+        //String firstName = "Curtis";
+        String firstName = console.readLine("What is your name? ");
 
         console.printf("hell, my name is %s\n",firstName);
         console.printf("%s is learning how to write in java\n",firstName);
